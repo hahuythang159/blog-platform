e2d4f33297469dc523dabeb5f08fe0ffd8541d95
thang <hahuythang159@gmail.com>
2025-03-16 17:29:40 +0700

    feat(auth): add email and username validation in registration

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -3,23 +3,36 @@ import bcrypt from "bcrypt";
 import User from "../models/User";
 import generateToken from "../utils/generateToken";
 
-export const registerUser = async (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response) : Promise<any> => {
     const { username, email, password } = req.body;
+
+    const existingEmail = await User.findOne({ email })
+
+    if(existingEmail){
+        return res.status(400).json({ message: "Email already exist "})
+    }
+
+    const existingUsername = await User.findOne({ username});
+
+    if(existingUsername){
+        return res.status(400).json({ message: "Username already exist"});
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({username, email, password: hashedPassword});
     await user.save();
     
-    res.status(201).json({ message: "User registered successfully"});
+    return res.status(201).json({ message: "User registered successfully"});
 };
 
-export const loginUser = async (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response): Promise<any> => {
     const { email, password} = req.body;
     const user = await User.findOne({email});
     
     if(user && (await bcrypt.compare(password, user.password))){
-        res.json({token: generateToken(user.id)});
+        return res.json({token: generateToken(user.id, user.username)});
     } else {
-        res.status(401).json({ message: "Invalid credentials"});
+        return res.status(401).json({ message: "Invalid credentials"});
     }
 }
