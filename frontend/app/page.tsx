90cca6d59b2698621fee82cdcd7de9ec5db1f663
thang <hahuythang159@gmail.com>
2025-03-16 17:19:55 +0700

    delete authSlice.ts, store.ts and change content in app page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-center p-6">
-      <h1 className="text-3xl font-bold">Welcome to My Blog</h1>
+      <h1 className="text-3xl font-bold">Welcome to Our Welsite</h1>
       <p>Read and share amazing stories!</p>
     </div>
   );
