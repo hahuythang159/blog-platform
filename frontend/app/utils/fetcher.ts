9b8fb9ff820186d2070ce686c819a5dd16058015
thang <hahuythang159@gmail.com>
2025-03-16 17:11:14 +0700

    feat: create fetcher function for API requests

---
+++ frontend/app/utils/fetcher.ts
@@ -0,0 +1,10 @@
+export const fetcher = async (url: string, options?: RequestInit) => {
+  const res = await fetch(`http://localhost:5000/${url}`, options);
+
+  if (res.status === 204) return null;
+
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.message || 'Something went wrong');
+  
+  return data;
+};
