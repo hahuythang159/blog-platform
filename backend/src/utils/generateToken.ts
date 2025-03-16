c7d99d464e1af6b7a77278a7a528bde5347af7af
thang <hahuythang159@gmail.com>
2025-03-16 17:30:25 +0700

    feat(auth): include username in JWT payload

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (id: string): string => {
-    return jwt.sign({ id }, process.env.JWT_SECRET as string, {expiresIn: "30d"});
+const generateToken = (id: string, username: string): string => {
+    return jwt.sign({ id, username }, process.env.JWT_SECRET as string, {expiresIn: "30d"});
 }
 
 export default generateToken;
