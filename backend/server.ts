a4c3f56854d389d49242b0fdf72debbd41453722
thang <hahuythang159@gmail.com>
2025-05-09 16:57:00 +0700

    chore(api): update comment route path to /api/comments

--- backend/server.ts
+++ backend/server.ts
@@ -33,7 +33,7 @@ app.use("/api/follow", followRoutes);
 
 app.use("/api/user", userRoutes);
 
-app.use("/api/comment", commentRoutes);
+app.use("/api/comments", commentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
