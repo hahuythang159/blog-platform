efefcd82b37c34bb51bf6eb0ebde5e43e4b1853d
thang <hahuythang159@gmail.com>
2025-05-15 18:53:45 +0700

    feat(post-cascade-delete): add deletion of related post stats when deleting a post

--- backend/src/middlewares/postCascadeDelete.ts
+++ backend/src/middlewares/postCascadeDelete.ts
@@ -1,5 +1,6 @@
 import { Schema } from "mongoose";
 import Comment from "../models/Comment";
+import PostStats from "../models/PostStats";
 
 /**
  * Middleware for Post: Delete all comments when deleting a post
@@ -8,9 +9,13 @@ const applyPostCascadeDelete = (PostSchema: Schema) => {
     PostSchema.pre("deleteOne", { document: true, query: false }, async function (next) {
         const postId = this._id;
         try {
+            // Delete related comments
             await Comment.deleteMany({ post: postId });
-        } catch (error) {
+            // Delete related post stats
+            await PostStats.deleteOne({ post: postId });
+        } catch (error: any) {
             console.error("Failed to delete comments when deleting post:", error);
+            return next(error);
         }
         next();
     });
