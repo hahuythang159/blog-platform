4e3cf7af9270a58f53e96e1a108f83fecb722f0f
thang <hahuythang159@gmail.com>
2025-06-22 14:53:49 +0700

    feat(cascade): add cascade delete for posts and users with orphan tag cleanup

--- backend/src/middlewares/postCascadeDelete.ts
+++ backend/src/middlewares/postCascadeDelete.ts
@@ -1,20 +1,35 @@
 import { Schema } from "mongoose";
 import Comment from "../models/Comment";
 import PostStats from "../models/PostStats";
+import Post from "../models/Post";
+import Tag from "../models/Tag";
 
 /**
- * Middleware for Post: Delete all comments when deleting a post
+ * Middleware for Post: Delete all comments, stats, and orphaned tags when deleting a post
  */
 const applyPostCascadeDelete = (PostSchema: Schema) => {
     PostSchema.pre("deleteOne", { document: true, query: false }, async function (next) {
         try {
             const postId = this._id;
+            
+            // Get tags information (if any)
+            const post = await Post.findById(postId).select("tags");
+            const tagIds: string[] = post?.tags?.map(tag => tag.toString()) || [];
+            
             await Promise.all([
                 // Delete related comments
                 Comment.deleteMany({ post: postId }),
                 // Delete related post stats
                 PostStats.deleteOne({ post: postId })
             ]);
+
+            // Count all posts using this tagId, if there are no posts using this tagId anymore then delete it
+            for (const tagId of tagIds) {
+                const count = await Post.countDocuments({ tags: tagId });
+                if (count === 0) {
+                    await Tag.deleteOne({ _id: tagId });
+                }
+            }
 
             next();
         } catch (error: any) {
--- backend/src/middlewares/userCascadeDelete.ts
+++ backend/src/middlewares/userCascadeDelete.ts
@@ -3,6 +3,7 @@ import Post from "../models/Post";
 import Comment from "../models/Comment";
 import Profile from "../models/Profile";
 import PostStats from "../models/PostStats";
+import Tag from "../models/Tag";
 
 /**
  * Middleware for User: Delete posts, comments, profiles,
@@ -13,8 +14,18 @@ const applyUserCascadeDelete = (UserSchema: Schema) => {
 
         try {
             const userId = this._id;
-            const userPosts = await Post.find({ author: userId }).select("_id");
+
+            // Get all posts of user
+            const userPosts = await Post.find({ author: userId }).select("_id tags");
             const postIds = userPosts.map(post => post._id);
+
+            // Collect all tags used in the post
+            const allTagIds = new Set<string>();
+            for (const post of userPosts) {
+                if (Array.isArray(post.tags)) {
+                    post.tags.forEach(tagId => allTagIds.add(tagId.toString()));
+                }
+            }
 
             await Promise.all([
                 Post.deleteMany({ author: userId }),
@@ -24,6 +35,21 @@ const applyUserCascadeDelete = (UserSchema: Schema) => {
                 Profile.updateMany({ followers: userId }, { $pull: { followers: userId } }),
                 Profile.updateMany({ following: userId }, { $pull: { following: userId } })
             ]);
+
+            // Check which tags have no more posts in use
+            const tagIdList = Array.from(allTagIds);
+            if (tagIdList.length > 0) {
+                const tagCheckTasks = tagIdList.map(tagId =>
+                    Post.countDocuments({ tags: tagId }).then(count => ({ tagId, count }))
+                );
+
+                const tagResults = await Promise.all(tagCheckTasks);
+                const orphanTags = tagResults.filter(tag => tag.count === 0).map(tag => tag.tagId);
+
+                if (orphanTags.length > 0) {
+                    await Tag.deleteMany({ _id: { $in: orphanTags } });
+                }
+            }
 
             next();
         } catch (error: any) {
